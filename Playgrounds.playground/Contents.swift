3cfb681c8c66042f46bb41825e70f002b156dc4b
zainnadeem <zn.nadeem@gmail.com>
2016-07-05 14:04:42 -0400

    playgrounds

--- Playgrounds.playground/Contents.swift
+++ Playgrounds.playground/Contents.swift
@@ -1,2 +1,4 @@
 let str = "Hello, world"
 str = "Goodbye, world"
+
+//Since you declared str to be a constant, try changing its value on the second line of the playground file. Notice that instead of executing that line of code and showing its output in the sidebar, Xcode instead highlights it as an error:
